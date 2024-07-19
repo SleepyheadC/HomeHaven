241cb53fed3210d707a13c3abbd1e312bb7c9830
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-07-19 15:56:44 +0530

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 
+
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -72,3 +73,13 @@ export const signin = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const signout = async (req, res, next) => {
+    try{
+      res.clearCookie('access_token');
+      res.status(200).json('User has been logged out!');
+  }
+  catch(error){
+    next(error);
+  }
+};
