1d5c31b799017a0e339636eb2556afd17321848d
Shreya Chaudhary <116176243+SleepyheadC@users.noreply.github.com>
2024-07-26 10:50:15 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className = 'py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className = 'text-3xl font-bold mb-4 text-slate-400'>
+        About HomeHaven
+      </h1>
+      <p className = 'mb-4 text-slate-700'>
+        Welcome to HomeHaven! Our mission is to make finding your perfect home an effortless and enjoyable experience. 
+        Whether you're looking to buy, rent, or explore the best property offers, HomeHaven is your trusted partner in the real estate journey. 
+        With a user-friendly interface and a vast selection of properties, we aim to simplify the process of discovering the place you'll love to call home. 
+        Our commitment to transparency, quality listings, and exceptional customer service sets us apart as the go-to destination for all your real estate needs.
+      </p>
+      <p className = 'mb-4 text-slate-700'>
+      At HomeHaven, we understand that a home is more than just a place to live—it's where memories are made, and dreams come true. 
+      That's why we've curated an extensive database of properties to suit every lifestyle and budget. From cozy apartments and family homes to luxury estates and investment properties, our platform offers a diverse range of options to help you find the perfect match. 
+      </p>
+      <p className = 'mb-4 text-slate-700'>
+      Our dedication to excellence extends beyond property listings. 
+      We believe in fostering a community where buyers, renters, and sellers can connect seamlessly. 
+      Our team of real estate professionals is always ready to provide personalized assistance and expert advice to guide you through every step of the process. 
+      At HomeHaven, we are not just helping you find a house—we are helping you find a place to belong. 
+      Join us today and discover how easy and enjoyable finding your dream home can be with HomeHaven.
+      </p>
+    </div>
   )
 }
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,8 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore, setShowMore] = useState(false);
+  // console.log(listings);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -53,6 +54,10 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+
+      if(data.length>5){
+        setShowMore(true);
+      }
       
       setListings(data);
       setLoading(false);
@@ -104,6 +109,20 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if(data.length<9){
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  }
 
   return (
     <div className='flex flex-col md:flex-row'>
@@ -217,6 +236,16 @@ export default function Search() {
           {!loading && listings && listings.map((listing) => (
             <ListingItem key={listing._id} listing={listing} />
           ))}
+
+          {showMore && (
+            <button onClick = {onShowMoreClick}
+            className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+            
+            
+          )}
         </div>
       </div>
     </div>
